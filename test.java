d9ac168e97a5b5961afb2fd2a85402862df166c0
kitkat99344 <kitkat99344@gmail.com>
2026-05-26 20:48:08 +0530

    done

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 package Aksh;
 
 public class test {
-public static void main(String[] args) {
-   int a=5;
-   int b=6;
-   int multiply=a*b;
-    System.out.println(multiply);
-System.out.println("done");
-}
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int multiply = a * b;
+        System.out.println(multiply);
+        System.out.println("dones");
+    }
 }
