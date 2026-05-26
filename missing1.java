5283f0f47743bef1ef0853640e3e2f45371d6fe5
kitkat99344 <kitkat99344@gmail.com>
2026-05-26 15:03:37 +0530

    multiply

---
+++ missing1.java
@@ -0,0 +1,16 @@
+package array;
+
+public class missing1 {
+    public static void main(String[] args){
+        int []arr={1,2,4,5,6};
+        int n=arr.length;
+        int SofN=(n*(n+1))/2;
+        int total=0;
+        for(int i=0;i<n;i++){
+            total+=arr[i];
+        }
+       
+        int miss=total-SofN;
+        System.out.println(miss);
+    }
+}
--- test.java
+++ test.java
@@ -1,5 +1,11 @@
 package Aksh;
 
 public class test {
+public static void main(String[] args) {
+   int a=5;
+   int b=6;
+   int multiply=a*b;
+    System.out.println(multiply);
 
+}
 }
